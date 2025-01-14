e8af972c4b671243bf3101df19a15fd2a9f6e55e
Kyanne Klein <kyanne@Kyannes-MacBook-Pro.local>
2025-01-14 15:56:51 -0700

    Made it a little harder

--- guesser.py
+++ guesser.py
@@ -3,10 +3,17 @@ print("Hi! I'm going to try to guess your age.")
 name = input("What is your name? ")
 
 guessed = False
+numberOfGuesses = 0
 while(guessed == False):
-    guess = random.randint(15,30)
+
+    guess = random.randint(10,30)
     user_response = input("Are you "+str(guess) + "years old?")
-    if user_response == 'y' or user_response == 'Y':
+    numberOfGuesses += 1
+    print(numberOfGuesses)
+    if numberOfGuesses == 5:
+        print("Man this game was too tough. I give up.")
+        guessed = True
+    elif user_response == 'y' or user_response == 'Y':
         print(f"haha! {name} is " + str(guess) + " yearse old! I guessed it!")
         guessed = True
     else:
